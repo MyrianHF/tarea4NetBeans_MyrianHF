1e86c0c4b18e074827222360a6eb816e55e1fb05
Myrian <myrianhf95@gmail.com>
2022-03-17 13:47:28 +0100

    Primer commit desde NetBeans

---
+++ src/main/java/es/iessanandres/entornos/tarea4_netbeans/Tarea4_NetBeans.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package es.iessanandres.entornos.tarea4_netbeans;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class Tarea4_NetBeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Iker!");
+    }
+}
